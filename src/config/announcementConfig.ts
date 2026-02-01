866591c62ab9be6b67e8e9cd4bd044b28481ae6a
ccd2s <tasaedem@outlook.com>
2026-02-02 06:14:34 +0800

    chore: 更新依赖及同步上游

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -1,13 +1,23 @@
 import type { AnnouncementConfig } from "../types/config";
 
 export const announcementConfig: AnnouncementConfig = {
-	title: "公告", // 公告标题
-	content: "欢迎！本博客已在近期转向静态，但目前可能会有 bug ，如果您遇到 bug 请给我反馈，谢谢！", // 公告内容
-	closable: true, // 允许用户关闭公告
+	// 公告标题
+	title: "公告",
+
+	// 公告内容
+	content: "欢迎！本博客已在近期转向静态，但目前可能会有 bug ，如果您遇到 bug 请给我反馈，谢谢！。",
+
+	// 是否允许用户关闭公告
+	closable: true,
+
 	link: {
-		enable: true, // 启用链接
-		text: "了解更多", // 链接文本
-		url: "/about/", // 链接 URL
-		external: true, // 内部链接
+		// 启用链接
+		enable: true,
+		// 链接文本
+		text: "了解更多",
+		// 链接 URL
+		url: "/about/",
+		// 内部链接
+		external: true,
 	},
 };
