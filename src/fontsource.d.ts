d01bec963131c4167fa07da44c56ccd3ac740df4
ccd2s <tasaedem@outlook.com>
2026-06-01 20:49:17 +0800

    fix: check

---
+++ src/fontsource.d.ts
@@ -0,0 +1,4 @@
+// @fontsource-variable packages only ship CSS, no type declarations
+declare module "@fontsource-variable/*";
+declare module "@fontsource-variable/jetbrains-mono";
+declare module "@rehype-callouts-theme";
